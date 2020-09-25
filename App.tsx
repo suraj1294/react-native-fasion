9c01f918b70ddb5add339ba41dc8d8de9d007145
Suraj Patil <suraj.patil@frontdoorhome.com>
2020-09-25 15:14:09 +0530

    add font-config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
 import "react-native-gesture-handler";
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { OnBoarding } from "./src/Authentication";
+import { LoadAssets } from "./src/components";
+
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+
+const AuthenticationStack = createStackNavigator();
 
 const AuthenticationNavigator = () => {
   return (
-    <View>
-      <Text>test</Text>
-    </View>
+    <AuthenticationStack.Navigator>
+      <AuthenticationStack.Screen name="OnBoarding" component={OnBoarding} />
+    </AuthenticationStack.Navigator>
   );
 };
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <LoadAssets {...{ fonts }}>
       <AuthenticationNavigator />
-    </NavigationContainer>
+    </LoadAssets>
   );
 }
---
+++ src/Authentication/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface OnBoardingProps {}
+
+const OnBoarding: React.FunctionComponent<OnBoardingProps> = () => {
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+};
+
+export default OnBoarding;
